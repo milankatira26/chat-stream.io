0eb2ebe2ce4ab244c46991e9339f4bf0bf989b8f
milankatira26 <milan@instaservice.com>
2024-02-26 19:37:45 +0530

    intial commit

---
+++ server/server.ts
@@ -0,0 +1,12 @@
+import { config } from "dotenv"
+config()
+
+import fastify from "fastify"
+import cors from "@fastify/cors"
+import { userRoutes } from "./routes/users"
+
+const app = fastify()
+app.register(cors, { origin: "*" })
+app.register(userRoutes)
+
+app.listen({ port: 5000 })
